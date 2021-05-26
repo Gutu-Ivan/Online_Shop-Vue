8ea418f49ca0ba21cfc75d117d35a6c2868deb8b
Gutu Ivan <gutsu.ivan@mail.ru>
2021-05-27 00:41:16 +0300

    Second commit

---
+++ src/components/v-catalog.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="v-catalog">
+    <div class="v-catalog__list">
+      <v-catalog-item
+          v-for="product in PRODUCTS"
+          :key="product.article"
+          :product_data="product"
+          @sendArticle="showChildArticleInConsole"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import vCatalogItem from './v-catalog-item'
+import {mapActions, mapGetters} from 'vuex'
+
+export default {
+  name: "v-catalog",
+  components: {
+    vCatalogItem
+  },
+  props: {},
+  data() {
+    return {
+
+    }
+  },
+  computed: {
+    ...mapGetters([
+        'PRODUCTS'
+    ])
+  },
+  methods: {
+    ...mapActions([
+        'GET_PRODUCTS_FROM_API'
+    ]),
+    showChildArticleInConsole(data) {
+      console.log(data)
+    }
+  },
+  mounted() {
+    this.GET_PRODUCTS_FROM_API()
+  }
+}
+</script>
+
+<style lang="scss">
+.v-catalog {
+  &__list {
+    display:flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    align-items: center;
+  }
+}
+</style>
---
+++ src/components/v-main-wrapper.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="v-main-wrapper">
+    <p>{{title}}</p>
+    <v-catalog/>
+    <v-cart/>
+  </div>
+</template>
+
+<script>
+import vCatalog from './v-catalog'
+import vCart from './v-cart'
+
+
+export default {
+  name: "v-main-wrapper",
+  components: {
+    vCatalog, vCart
+  },
+  props: {},
+  data() {
+    return {
+      title: 'Catalog'
+    }
+  }
+}
+</script>
+
+<style scoped>
+.v-main-wrapper{
+  max-width: 900px;
+  margin: 0 auto;
+}
+</style>
